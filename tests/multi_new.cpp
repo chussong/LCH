d4247134efb33fbe5c792d801702fe9f26e8561e
chussong <charles.hussong@gmail.com>
2019-07-11 16:26:52 +0900

    Add unit test for multi_new

---
+++ tests/multi_new.cpp
@@ -0,0 +1,58 @@
+#include "multi_new.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+// Copyright 2018-2019 by Joyz Inc of Tokyo, Japan (author: Charles Hussong) //
+//                                                                           //
+// Licensed under the Apache License, Version 2.0 (the "License");           //
+// you may not use this file except in compliance with the License.          //
+// You may obtain a copy of the License at                                   //
+//                                                                           //
+//    http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                           //
+// Unless required by applicable law or agreed to in writing, software       //
+// distributed under the License is distributed on an "AS IS" BASIS,         //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  //
+// See the License for the specific language governing permissions and       //
+// limitations under the License.                                            //
+///////////////////////////////////////////////////////////////////////////////
+
+#include "Catch2/catch.hpp"
+
+#include <stdexcept>
+#include <array>
+#include <iostream>
+
+class ThrowAfterThree {
+  public:
+    ~ThrowAfterThree() {
+        count -= 1;
+    }
+
+    static void* operator new(std::size_t size) {
+        if (count < 3) {
+            count += 1;
+            return ::operator new(size);
+        } else {
+            throw std::exception();
+        }
+    }
+
+    static int Count() {
+        return count;
+    }
+
+  private:
+    static int count;
+};
+
+int ThrowAfterThree::count = 0;
+
+TEST_CASE("multi_new works and is properly exception safe", "[multi_new]") {
+    ThrowAfterThree* a;
+    ThrowAfterThree* b;
+    ThrowAfterThree* c;
+    ThrowAfterThree* d;
+    REQUIRE_THROWS_AS(LCH::MultiNew(a, b, c, d), std::exception);
+    // if the count is 0 after this, they have been correctly destroyed
+    REQUIRE(ThrowAfterThree::Count() == 0);
+}
