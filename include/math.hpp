4771e9e78be6774b6526c380e276258e67844ec0
chussong <charles.hussong@gmail.com>
2019-07-26 11:21:16 +0900

    Add stats functions without Bessel's correction

--- include/math.hpp
+++ include/math.hpp
@@ -1,4 +1,10 @@
 // math.hpp: generally applicable math functions not found in the stdlib.
+//
+// For functions which take a standard deviation, there is an argument isSample
+// (defaults to true). If the range being passed to the function is a sample of
+// a larger population, Bessel's correction must be used in computing the 
+// standard deviation; if the range is the entire population, set isSample to
+// false to avoid Bessel's correction.
 
 ///////////////////////////////////////////////////////////////////////////////
 // Copyright 2018-2019 by Joyz Inc of Tokyo, Japan (author: Charles Hussong) //
@@ -60,36 +66,37 @@ void SubtractMean(Container& container) {
 // if the data in [begin, end) *has zero mean already*, standardize it by 
 // dividing by the standard deviation
 template<typename Iterator>
-void StandardizeFromZeroMean(Iterator begin, Iterator end) {
+void StandardizeFromZeroMean(Iterator begin, Iterator end, bool isSample = true) {
     using T = typename std::iterator_traits<Iterator>::value_type;
     if (std::next(begin) == end) return;
 
-    T variance = std::inner_product(begin, end, begin, T())
-               / (std::distance(begin, end) - 1); // -1 for Bessel's correction
+    // if this is a sample, we need Bessel's correction
+    T denominator = std::distance(begin, end) - (isSample ? 1 : 0);
+    T variance = std::inner_product(begin, end, begin, T()) / denominator;
     T sd = std::sqrt(variance);
     std::transform(begin, end, begin, [sd](const T& val){ return val/sd; });
 }
 
 // standardize zero-mean data in a container with a begin() and an end()
 template<typename Container>
-void StandardizeFromZeroMean(Container& container) {
-    StandardizeFromZeroMean(container.begin(), container.end());
+void StandardizeFromZeroMean(Container& container, bool isSample = true) {
+    StandardizeFromZeroMean(container.begin(), container.end(), isSample);
 }
 
 // standardize the data in [begin, end) by subtacting the mean and dividing by
 // the standard deviation
 template<typename Iterator>
-void Standardize(Iterator begin, Iterator end) {
+void Standardize(Iterator begin, Iterator end, bool isSample = true) {
     if (begin == end) return;
 
     SubtractMean(begin, end);
-    StandardizeFromZeroMean(begin, end);
+    StandardizeFromZeroMean(begin, end, isSample);
 }
 
 // standardize the data in a container with a begin() and an end()
 template<typename Container>
-void Standardize(Container& container) {
-    Standardize(container.begin(), container.end());
+void Standardize(Container& container, bool isSample = true) {
+    Standardize(container.begin(), container.end(), isSample);
 }
 
 // get the "standard scores" in the statistical sense of a number of standard
@@ -97,31 +104,31 @@ void Standardize(Container& container) {
 // standardize the copy
 template<typename Iterator>
 std::vector<typename std::iterator_traits<Iterator>::value_type> 
-StandardScores(Iterator begin, Iterator end) {
+StandardScores(Iterator begin, Iterator end, bool isSample = true) {
     using T = typename std::iterator_traits<Iterator>::value_type;
     std::vector<T> output;
     output.reserve(std::distance(begin, end));
     std::copy(begin, end, std::back_inserter(output));
-    Standardize(output.begin(), output.end());
+    Standardize(output.begin(), output.end(), isSample);
     return output;
 }
 
 // get the standard scores of the given container's contents without modifying
 template<typename Container>
-Container StandardScores(const Container& container) {
+Container StandardScores(const Container& container, bool isSample = true) {
     Container output;
     std::copy(container.begin(), container.end(), std::back_inserter(output));
-    Standardize(output.begin(), output.end());
+    Standardize(output.begin(), output.end(), isSample);
     return output;
 }
 
 // partial specialization of above for vectors since they can be reserved
 template<typename T>
-std::vector<T> StandardScores(const std::vector<T>& container) {
+std::vector<T> StandardScores(const std::vector<T>& container, bool isSample = true) {
     std::vector<T> output;
     output.reserve(container.size());
     std::copy(container.begin(), container.end(), std::back_inserter(output));
-    Standardize(output.begin(), output.end());
+    Standardize(output.begin(), output.end(), isSample);
     return output;
 }
 
@@ -129,7 +136,7 @@ std::vector<T> StandardScores(const std::vector<T>& container) {
 // changing the original data
 template<typename Iterator>
 std::array<typename std::iterator_traits<Iterator>::value_type,2> 
-MeanAndStdDev(Iterator begin, Iterator end) {
+MeanAndStdDev(Iterator begin, Iterator end, bool isSample = true) {
     using T = typename std::iterator_traits<Iterator>::value_type;
     std::array<T,2> meanAndStdDev {0.0, 0.0};
     if (begin == end) return meanAndStdDev;
@@ -143,7 +150,7 @@ MeanAndStdDev(Iterator begin, Iterator end) {
     meanAndStdDev[0] = *begin - firstValue;
     if (deviations.size() <= 1) return meanAndStdDev;
 
-    StandardizeFromZeroMean(deviations.begin(), deviations.end());
+    StandardizeFromZeroMean(deviations.begin(), deviations.end(), isSample);
     meanAndStdDev[1] = firstValue / deviations[0];
 
     return meanAndStdDev;
--- tests/math.cpp
+++ tests/math.cpp
@@ -37,6 +37,7 @@ static constexpr std::array<double,10> input {
 
 static constexpr double mean = 2.67168;
 static constexpr double stdDev = 5.95056;
+static constexpr double stdDevNonSample = 5.64519;
 
 static constexpr std::array<double,10> standardized {
     0.889204,
@@ -49,6 +50,19 @@ static constexpr std::array<double,10> standardized {
     -1.13907,
     0.469171,
     -1.31908
+};
+
+static constexpr std::array<double,10> standardizedNonSample {
+    0.937303, 
+    -0.243452, 
+    -1.69768, 
+    0.475449, 
+    0.717697, 
+    0.935775,
+    0.971469, 
+    -1.20068, 
+    0.49455, 
+    -1.39043
 };
 
 bool CloseEnough(double x, double y) {
@@ -68,31 +82,43 @@ bool CloseEnough(ContainerA x, ContainerB y) {
 }
 
 TEST_CASE("math routines work", "[math]") {
-    SECTION("math works with iterators") {
-        REQUIRE(CloseEnough(LCH::Mean(input.cbegin(), input.cend()), mean));
+    SECTION("statistics work with iterators and Bessel's correction") {
+        CHECK(CloseEnough(LCH::Mean(input.cbegin(), input.cend()), mean));
         std::array<double,10> inputCopy = input;
         LCH::Standardize(inputCopy.begin(), inputCopy.end());
-        REQUIRE(CloseEnough(inputCopy, standardized));
+        CHECK(CloseEnough(inputCopy, standardized));
         auto standardScores = LCH::StandardScores(input.cbegin(), input.cend());
-        REQUIRE(CloseEnough(standardScores, inputCopy));
+        CHECK(CloseEnough(standardScores, inputCopy));
 
         auto meanAndStdDev = LCH::MeanAndStdDev(input.cbegin(), input.cend());
-        REQUIRE(CloseEnough(meanAndStdDev[0], mean));
-        REQUIRE(CloseEnough(meanAndStdDev[1], stdDev));
+        CHECK(CloseEnough(meanAndStdDev[0], mean));
+        CHECK(CloseEnough(meanAndStdDev[1], stdDev));
     }
 
-    SECTION("math works with pointers") {
-        REQUIRE(CloseEnough(LCH::Mean(input.data(), input.data() + input.size()), mean));
+    SECTION("statistics work with pointers and Bessel's correction") {
+        CHECK(CloseEnough(LCH::Mean(input.data(), input.data() + input.size()), mean));
         std::array<double,10> inputCopy = input;
         LCH::Standardize(inputCopy.data(), inputCopy.data() + inputCopy.size());
-        REQUIRE(CloseEnough(inputCopy, standardized));
+        CHECK(CloseEnough(inputCopy, standardized));
         auto standardScores = LCH::StandardScores(input.data(), 
                                                   input.data() + input.size());
-        REQUIRE(CloseEnough(standardScores, inputCopy));
+        CHECK(CloseEnough(standardScores, inputCopy));
 
         auto meanAndStdDev = LCH::MeanAndStdDev(input.data(), 
                                                 input.data() + input.size());
-        REQUIRE(CloseEnough(meanAndStdDev[0], mean));
-        REQUIRE(CloseEnough(meanAndStdDev[1], stdDev));
+        CHECK(CloseEnough(meanAndStdDev[0], mean));
+        CHECK(CloseEnough(meanAndStdDev[1], stdDev));
+    }
+
+    SECTION("statistics work without Bessel's correction") {
+        std::array<double,10> inputCopy = input;
+        LCH::Standardize(inputCopy.begin(), inputCopy.end(), false);
+        CHECK(CloseEnough(inputCopy, standardizedNonSample));
+        auto standardScores = LCH::StandardScores(input.cbegin(), input.cend(), false);
+        CHECK(CloseEnough(standardScores, inputCopy));
+
+        auto meanAndStdDev = LCH::MeanAndStdDev(input.cbegin(), input.cend(), false);
+        CHECK(CloseEnough(meanAndStdDev[0], mean));
+        CHECK(CloseEnough(meanAndStdDev[1], stdDevNonSample));
     }
 }
